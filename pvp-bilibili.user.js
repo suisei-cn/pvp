59fe472df641340cea1c57ec4149b5efe6ebdf42
Outvi V <git@outv.im>
2020-08-13 00:27:15 +0800

    feat(bilibili): Add cut feature

--- pvp-bilibili.user.js
+++ pvp-bilibili.user.js
@@ -7,218 +7,256 @@
 // @match       https://bilibili.com/video/*
 // @match       https://www.bilibili.com/video/*
 // @grant       none
-// @version     0.5.8
+// @version     0.7.0
 // @author      Outvi V
 // ==/UserScript==
 
-'use strict'
+"use strict";
 
-console.log('Precise Video Playback is up')
+function createCutButton(time, videoElement) {
+  let btnJump = document.createElement("button");
+  let btnRemove = document.createElement("button");
+  let btnContainer = document.createElement("div");
+  btnJump.innerText = time;
+  btnRemove.innerText = "x";
+  btnJump.addEventListener("click", () => {
+    videoElement.currentTime = time;
+  });
+  btnRemove.addEventListener("click", () => {
+    btnContainer.style.display = "none";
+  });
+  applyStyle(btnContainer, {
+    marginRight: "0.5vw",
+    flexShrink: "0",
+  });
+  btnContainer.append(btnJump, btnRemove);
+  return btnContainer;
+}
+
+console.log("Precise Video Playback is up");
 
 function getVideoId(url) {
-  return String(url).match(/(a|b)v([^?&#]+)/i)[0]
+  return String(url).match(/(a|b)v([^?&#]+)/i)[0];
 }
 
 function applyStyle(elem, styles) {
   for (const [key, value] of Object.entries(styles)) {
-    elem.style[key] = value
+    elem.style[key] = value;
   }
 }
 
 function parseTime(str) {
-  if (!isNaN(Number(str))) return Number(str)
-  let time = str.match(/([0-9]?)?:([0-9]+)(\.([0-9]+))?/)
-  if (time === null) return -1
+  if (!isNaN(Number(str))) return Number(str);
+  let time = str.match(/([0-9]?)?:([0-9]+)(\.([0-9]+))?/);
+  if (time === null) return -1;
   let ret =
-    Number(time[1] || 0) * 60 + Number(time[2]) + Number(time[4] || 0) * 0.1
-  if (ret == NaN) return -1
-  return ret
+    Number(time[1] || 0) * 60 + Number(time[2]) + Number(time[4] || 0) * 0.1;
+  if (ret == NaN) return -1;
+  return ret;
 }
 
 function generateControl() {
-  let app = document.createElement('div')
-  let inputFrom = document.createElement('input')
-  inputFrom.placeholder = 'from time'
-  let inputTo = document.createElement('input')
-  inputTo.placeholder = 'to time'
-  let currentTime = document.createElement('span')
-  let btn = document.createElement('button')
-  let btnStop = document.createElement('button')
-  let btnExport = document.createElement('button')
+  let app = document.createElement("div");
+  let cutBar = document.createElement("div");
+  let inputFrom = document.createElement("input");
+  inputFrom.placeholder = "from time";
+  let inputTo = document.createElement("input");
+  inputTo.placeholder = "to time";
+  let currentTime = document.createElement("span");
+  let btn = document.createElement("button");
+  let btnStop = document.createElement("button");
+  let btnExport = document.createElement("button");
+  let btnCut = document.createElement("button");
   applyStyle(app, {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    maxWidth: '600px',
-    marginTop: '15px',
-    marginLeft: 'auto',
-    marginRight: 'auto',
-  })
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    maxWidth: "600px",
+    marginTop: "15px",
+    marginLeft: "auto",
+    marginRight: "auto",
+  });
+  applyStyle(cutBar, {
+    display: "flex",
+    flexWrap: "wrap",
+    marginTop: "1.5vh",
+  });
   applyStyle(currentTime, {
-    fontSize: '1.2rem',
-    minWidth: '7.5rem',
-    textAlign: 'center',
-  })
+    fontSize: "1.2rem",
+    minWidth: "7.5rem",
+    textAlign: "center",
+  });
   let inputCommonStyle = {
-    width: '80px',
-  }
-  applyStyle(inputFrom, inputCommonStyle)
-  applyStyle(inputTo, inputCommonStyle)
-  btn.innerText = 'Repeat play'
-  btnStop.innerText = 'Stop'
-  btnExport.innerText = 'Export'
-  app.appendChild(inputFrom)
-  app.appendChild(inputTo)
-  app.appendChild(currentTime)
-  app.appendChild(btn)
-  app.appendChild(btnStop)
-  app.appendChild(btnExport)
+    width: "80px",
+  };
+  applyStyle(inputFrom, inputCommonStyle);
+  applyStyle(inputTo, inputCommonStyle);
+  btn.innerText = "Repeat play";
+  btnStop.innerText = "Stop";
+  btnExport.innerText = "Export";
+  btnCut.innerText = "Cut";
+  app.appendChild(inputFrom);
+  app.appendChild(inputTo);
+  app.appendChild(currentTime);
+  app.appendChild(btn);
+  app.appendChild(btnStop);
+  app.appendChild(btnExport);
+  app.appendChild(btnCut);
   return {
     app,
+    cutBar,
     inputFrom,
     inputTo,
     currentTime,
     btn,
     btnStop,
     btnExport,
-  }
+    btnCut,
+  };
 }
 
 async function sleep(time) {
   await new Promise((resolve) => {
     setTimeout(() => {
-      resolve()
-    }, time)
-  })
+      resolve();
+    }, time);
+  });
 }
 
 async function waitfor(cb) {
   while (true) {
-    if (cb()) return
-    await sleep(500)
+    if (cb()) return;
+    await sleep(500);
   }
 }
 
 async function main() {
-  console.log('Waiting for the page...')
+  console.log("Waiting for the page...");
   // Wait for Bilibili to fully render the page
   // Or error could occur after inserting our widget
 
   await waitfor(() => {
     return (
-      document.querySelector('#v_upinfo .up-face') ||
-      document.querySelector('#member-container .avatar')
-    )
-  })
-  console.log('Pre-install hook OK')
+      document.querySelector("#v_upinfo .up-face") ||
+      document.querySelector("#member-container .avatar")
+    );
+  });
+  console.log("Pre-install hook OK");
 
   // Player fetching
-  console.log('Waiting for the player...')
-  let anchor
+  console.log("Waiting for the player...");
+  let anchor;
   while (true) {
-    anchor = document.querySelector('#v_desc')
-    if (anchor && !anchor.hidden) break
-    await sleep(500)
+    anchor = document.querySelector("#v_desc");
+    if (anchor && !anchor.hidden) break;
+    await sleep(500);
   }
-  let videoElement = document.querySelector('video')
+  let videoElement = document.querySelector("video");
   if (!videoElement || !anchor) {
-    console.warn('Player not found. Exiting.')
-    return
+    console.warn("Player not found. Exiting.");
+    return;
   }
-  console.log('Player detected.')
+  console.log("Player detected.");
 
   // Layout
-  let control = generateControl()
-  let shadowParent = document.createElement('div')
-  let shadow = shadowParent.attachShadow({ mode: 'open' })
-  anchor.parentElement.insertBefore(shadowParent, anchor)
-  shadow.appendChild(control.app)
+  let control = generateControl();
+  let shadowParent = document.createElement("div");
+  let shadow = shadowParent.attachShadow({ mode: "open" });
+  anchor.parentElement.insertBefore(shadowParent, anchor);
+  anchor.parentElement.insertBefore(control.cutBar, anchor);
+  shadow.appendChild(control.app);
 
   // States
   let fromValue = 0,
-    toValue = 0
+    toValue = 0;
 
   // Initial state update attempt
   let urlTime = window.location.hash.match(
     /#pvp([0-9]+\.?[0-9]?)-([0-9]+\.?[0-9]?)/
-  )
+  );
   if (urlTime !== null) {
-    console.log('Attempting to recover time from URL...')
-    control.inputFrom.value = fromValue = Number(urlTime[1]) || 0
-    control.inputTo.value = toValue = Number(urlTime[2]) || 0
+    console.log("Attempting to recover time from URL...");
+    control.inputFrom.value = fromValue = Number(urlTime[1]) || 0;
+    control.inputTo.value = toValue = Number(urlTime[2]) || 0;
   }
 
   // Current playback time
   function updateCurrentTime() {
-    control.currentTime.innerText = Number(videoElement.currentTime).toFixed(2)
-    requestAnimationFrame(updateCurrentTime)
+    control.currentTime.innerText = Number(videoElement.currentTime).toFixed(2);
+    requestAnimationFrame(updateCurrentTime);
   }
-  requestAnimationFrame(updateCurrentTime)
+  requestAnimationFrame(updateCurrentTime);
 
   // Repeat playback
   function onTimeUpdate() {
     if (videoElement.currentTime >= Number(toValue)) {
-      videoElement.currentTime = Number(fromValue)
+      videoElement.currentTime = Number(fromValue);
     }
   }
-  control.btn.addEventListener('click', (evt) => {
-    evt.preventDefault()
-    videoElement.pause()
-    videoElement.currentTime = fromValue
+  control.btn.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    videoElement.pause();
+    videoElement.currentTime = fromValue;
     if (fromValue < toValue) {
-      videoElement.play()
-      videoElement.addEventListener('timeupdate', onTimeUpdate)
+      videoElement.play();
+      videoElement.addEventListener("timeupdate", onTimeUpdate);
     } else {
-      videoElement.removeEventListener('timeupdate', onTimeUpdate)
+      videoElement.removeEventListener("timeupdate", onTimeUpdate);
     }
-  })
-  control.btnStop.addEventListener('click', (evt) => {
-    evt.preventDefault()
-    videoElement.removeEventListener('timeupdate', onTimeUpdate)
-    videoElement.pause()
-  })
+  });
+  control.btnStop.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    videoElement.removeEventListener("timeupdate", onTimeUpdate);
+    videoElement.pause();
+  });
+
+  control.btnCut.addEventListener("click", () => {
+    let nowTime = Number(videoElement.currentTime).toFixed(2);
+    let btn = createCutButton(nowTime, videoElement);
+    control.cutBar.append(btn);
+  });
 
   // Start/end time setting
   function updateURL() {
-    history.pushState(null, null, `#pvp${fromValue}-${toValue}`)
+    history.pushState(null, null, `#pvp${fromValue}-${toValue}`);
   }
-  control.inputFrom.addEventListener('change', () => {
-    let input = control.inputFrom.value
-    if (input === '') {
-      fromValue = 0
-      control.inputFrom.placeholder = 'from 0'
-      return
+  control.inputFrom.addEventListener("change", () => {
+    let input = control.inputFrom.value;
+    if (input === "") {
+      fromValue = 0;
+      control.inputFrom.placeholder = "from 0";
+      return;
     }
-    let time = parseTime(input)
+    let time = parseTime(input);
     if (time == -1) {
-      control.btn.disabled = true
-      return
+      control.btn.disabled = true;
+      return;
     }
-    control.btn.disabled = false
-    fromValue = time
-    updateURL()
-  })
-  control.inputTo.addEventListener('change', () => {
-    let input = control.inputTo.value
-    if (input === '') {
-      toValue = videoElement.duration || 0
-      control.inputTo.placeholder = `to ${toValue.toFixed(2)}`
-      return
+    control.btn.disabled = false;
+    fromValue = time;
+    updateURL();
+  });
+  control.inputTo.addEventListener("change", () => {
+    let input = control.inputTo.value;
+    if (input === "") {
+      toValue = videoElement.duration || 0;
+      control.inputTo.placeholder = `to ${toValue.toFixed(2)}`;
+      return;
     }
-    let time = parseTime(input)
+    let time = parseTime(input);
     if (time == -1) {
-      control.btn.disabled = true
-      return
+      control.btn.disabled = true;
+      return;
     }
-    control.btn.disabled = false
-    toValue = time
-    updateURL()
-  })
+    control.btn.disabled = false;
+    toValue = time;
+    updateURL();
+  });
 
   // Button export
-  control.btnExport.addEventListener('click', (evt) => {
-    evt.preventDefault()
-    let videoId = getVideoId(window.location)
+  control.btnExport.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    let videoId = getVideoId(window.location);
     alert(`ykdl https://www.bilibili.com/video/${videoId} -O output-${videoId} &&
     ffmpeg -i output-${videoId}.flv \
 -ss ${fromValue} \
@@ -227,8 +265,8 @@ async function main() {
 -ab 192k \
 -af loudnorm=I=-16:TP=-2:LRA=11 \
 -vn \
-output-${videoId}-${fromValue}-${toValue}.mp3 && rm output-${videoId}.flv`)
-  })
+output-${videoId}-${fromValue}-${toValue}.mp3 && rm output-${videoId}.flv`);
+  });
 }
 
-main()
+main();
